be955c93948487487d0af59d04a3a3f9e9576b41
IoanAdafinei <ioan_adafinei@yahoo.com>
2023-01-06 22:00:40 +0200

    Added 5 day forecast and other small improvements

--- scripts/script1.js
+++ scripts/script1.js
@@ -29,6 +29,10 @@ function cleargallery() {
   document.querySelector(".display_images").innerHTML = "";
 }
 
+function clearForecast() {
+  document.querySelector("#weather-forecast").innerHTML = "";
+}
+
 function fetchWeatherData(location) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=351a7599b0398e628c3c643a1f8d4ac5`
@@ -45,16 +49,45 @@ function fetchWeatherData(location) {
       weatherDataElement.innerHTML = `
               <p class="centered">The weather in ${location} is currently ${
         data.weather[0].description
-      } </p><p class="temp"><img src = "http://openweathermap.org/img/wn/${id_condition}@2x.png">
+      } </p><p class="temp"><img src = "http://openweathermap.org/img/wn/${id_condition}@2x.png" class="conditions">
               ${tmp - 273}°C</p>
             `;
     });
 }
 
+function fetchForecastData(location) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=351a7599b0398e628c3c643a1f8d4ac5&cnt=5`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherDataElement = document.querySelector("#weather-forecast");
+      clearForecast();
+      data.list.forEach((element) => {
+        let t = element.main.temp;
+        let id_condition = element.weather[0].icon;
+        let tmp = t.toFixed();
+        let date = new Date(element.dt * 1000);
+        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let name = days[date.getDay()];
+
+        let dayBlock = document.createElement("div");
+        dayBlock.id = "weather-forecast";
+        dayBlock.innerHTML = `
+              <p class="centered">${name}</p>
+              <p class="temp"><img src = "http://openweathermap.org/img/wn/${id_condition}@2x.png" class="conditions">
+              ${tmp - 273}°C</p>
+            `;
+        document.querySelector("#weather-forecast").appendChild(dayBlock);
+      });
+    });
+}
+
 function initialize() {
   document.body.style.padding = "20px";
-  document.body.style.paddingLeft = "13vw"
+  document.body.style.paddingLeft = "13vw";
 }
 
 initialize();
 fetchWeatherData("Bucharest");
+fetchForecastData("Bucharest");
